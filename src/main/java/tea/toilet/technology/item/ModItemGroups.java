1723dfc8c55890acbe2088281e825e24bd80bb0d
茶白 <1332709015@qq.com>
2026-07-17 18:37:11 +0800

    构建了mod的一个个物品与它的创造物品栏

---
+++ src/main/java/tea/toilet/technology/item/ModItemGroups.java
@@ -0,0 +1,48 @@
+package tea.toilet.technology.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tea.toilet.technology.ToiletTechnology;
+
+
+public class ModItemGroups {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ModItemGroups.class);
+
+    // 创建一个厕所工艺物品栏
+    public static final ResourceKey<CreativeModeTab> TOILET_TECHNOLOGY_GROUP_KEY = ResourceKey.create(BuiltInRegistries.CREATIVE_MODE_TAB.key(), ResourceLocation.fromNamespaceAndPath(ToiletTechnology.MOD_ID, ToiletTechnology.MOD_ID));
+    public static final CreativeModeTab TOILET_TECHNOLOGY_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ModItems.FECES))// 标签页图标物品
+            .title(Component.translatable("itemGroup.toilet_technology"))
+            .build();
+
+    /**
+     * 创建物品标签页
+     */
+    private static ResourceKey<CreativeModeTab> createKey(String string) {
+        return ResourceKey.create(Registries.CREATIVE_MODE_TAB, ResourceLocation.withDefaultNamespace(string));
+    }
+
+    /**
+     * 注册物品标签页
+     */
+    public static void registerModItemsGroups(){
+        LOGGER.info("toilet_technology Items Group registered");
+        // 注册物品栏
+        Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, TOILET_TECHNOLOGY_GROUP_KEY, TOILET_TECHNOLOGY_GROUP);
+        // 将物品添加到厕所工艺标签页里
+        ItemGroupEvents.modifyEntriesEvent(TOILET_TECHNOLOGY_GROUP_KEY)
+                .register(ModItems::addItemToIG);
+    }
+
+}

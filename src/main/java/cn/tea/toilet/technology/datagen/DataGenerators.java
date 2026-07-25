ee5a4ec0a29047823e55af9a2b7c5596356c9763
茶白 <1332709015@qq.com>
2026-07-25 12:34:44 +0800

    沼气可以参与燃气发电机的发电

--- src/main/java/cn/tea/toilet/technology/datagen/DataGenerators.java
+++ src/main/java/cn/tea/toilet/technology/datagen/DataGenerators.java
@@ -36,6 +36,7 @@ public class DataGenerators {
 
         generator.addProvider(event.includeServer(), new ModRecipeProvider(output, lookupProvider));
         generator.addProvider(event.includeServer(), new ModDataMapProvider(output, lookupProvider));
+        generator.addProvider(event.includeServer(), new MekanismGeneratorCompatDataProvider(output));
 
         generator.addProvider(event.includeClient(), new ModLanguageEnUsProvider(output, "en_us"));
         generator.addProvider(event.includeClient(), new ModLanguageZhCnProvider(output, "zh_cn"));
---
+++ src/main/java/cn/tea/toilet/technology/datagen/MekanismGeneratorCompatDataProvider.java
@@ -0,0 +1,53 @@
+package cn.tea.toilet.technology.datagen;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import cn.tea.toilet.technology.gas.GasRegistry;
+import java.nio.file.Path;
+import java.util.concurrent.CompletableFuture;
+import net.minecraft.data.CachedOutput;
+import net.minecraft.data.DataProvider;
+import net.minecraft.data.PackOutput;
+
+/** Generates optional Mekanism-compatible data without linking against its API. */
+public final class MekanismGeneratorCompatDataProvider implements DataProvider {
+    private static final int BIOGAS_BURN_TICKS = 20;
+    private static final long BIOGAS_ENERGY_PER_TICK = 200;
+
+    private final PackOutput output;
+
+    public MekanismGeneratorCompatDataProvider(PackOutput output) {
+        this.output = output;
+    }
+
+    @Override
+    public CompletableFuture<?> run(CachedOutput cachedOutput) {
+        String biogasId = GasRegistry.BIOGAS.id().toString();
+        Path gaseousTag = output.getOutputFolder()
+                .resolve("data/mekanism/tags/mekanism/chemical/gaseous.json");
+        Path fuelDataMap = output.getOutputFolder()
+                .resolve("data/mekanism/data_maps/mekanism/chemical/chemical_attribute_fuel.json");
+
+        JsonObject gaseous = new JsonObject();
+        JsonArray gaseousValues = new JsonArray();
+        gaseousValues.add(biogasId);
+        gaseous.add("values", gaseousValues);
+
+        JsonObject fuel = new JsonObject();
+        JsonObject values = new JsonObject();
+        JsonObject biogasFuel = new JsonObject();
+        biogasFuel.addProperty("burn_time", BIOGAS_BURN_TICKS);
+        biogasFuel.addProperty("energy", BIOGAS_ENERGY_PER_TICK);
+        values.add(biogasId, biogasFuel);
+        fuel.add("values", values);
+
+        return CompletableFuture.allOf(
+                DataProvider.saveStable(cachedOutput, gaseous, gaseousTag),
+                DataProvider.saveStable(cachedOutput, fuel, fuelDataMap));
+    }
+
+    @Override
+    public String getName() {
+        return "Mekanism Generators compatibility";
+    }
+}
